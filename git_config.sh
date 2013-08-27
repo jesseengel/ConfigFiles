baf24d5004b6da917d75a495800538c9073307c2
jesseengel <jesse.engel@gmail.com>
2013-08-27 11:48:24 -0700

    Tried to get evil added as a submodule

--- git_config.sh
+++ git_config.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-git add .emacs.d/
+git add -A .emacs.d/
 git add .emacs
 git add .bash_profile
 git add .bashrc
 git add git_config.sh
+git add .gitignore
 
 git rm --cached -r .emacs.d/.saves/
 git rm --cached -r .emacs.d/auto-save-list/
